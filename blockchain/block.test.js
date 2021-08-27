6860d40b0579768bcc52cc026a10a18d16c7fc06
Tristan <trung3t123@gmail.com>
2021-08-27 16:26:11 +0700

    master

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe("Block", () => {
 
@@ -8,7 +9,6 @@ describe("Block", () => {
     data = 'bar';
     lastBlock = Block.genesis();
     block = Block.mineBlock(lastBlock, data);
-
   })
 
   it('sets the `data` to match the input', () => {
@@ -17,5 +17,18 @@ describe("Block", () => {
 
   it('sets the `lastHash` to match the hash of the last block', () => {
     expect(block.lastHash).toEqual(lastBlock.hash)
+  })
+
+  it('generates a hash that matches the difficulty', () => {
+    expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty))
+    console.log('nonce', block)
+  })
+
+  it('lowers the difficulty for slowly mined blocks', () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 360000)).toEqual(block.difficulty - 1);
+  })
+
+  it('raise the difficulty for quickly mined blocks', () => {
+    expect(Block.adjustDifficulty(block, block.timestamp + 1)).toEqual(block.difficulty + 1);
   })
 })
